79c62f1a3c0be989d1171b186ab84380e8aea4f5
BrainInBlack <braininblack@gmail.com>
2022-05-21 14:32:47 +0200

    Old Update

--- Compensation.lua
+++ Compensation.lua
@@ -1,8 +1,7 @@
 local f=CreateFrame("Frame")
 f:SetScript("OnEvent", function(self, event)
 	local down, up, lagHome, lagWorld = GetNetStats()
-	SetCVar("SpellQueueWindow", 1)
-	SetCVar("MaxSpellStartRecoveryOffset", lagWorld)
+	SetCVar("SpellQueueWindow", lagWorld)
 end);
 f:RegisterEvent("ZONE_CHANGED")
 f:RegisterEvent("ADDON_LOADED")
